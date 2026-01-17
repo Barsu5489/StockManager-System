d1e17cf76c4563b3187bd98448cef139492de5b9
Barsu5489 <ebarsulai@gmail.com>
2026-01-17 12:42:16 +0300

    feat: Display amber alert lising all low-stock products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    @low_stock_products = Product.low_stock
   end
 
   # GET /products/1 or /products/1.json
