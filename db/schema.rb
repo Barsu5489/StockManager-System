179e642a24dab46647160ffd3328bef42c3ce7e3
Barsu5489 <ebarsulai@gmail.com>
2026-01-17 12:15:25 +0300

    feat: add Turbo streams for real-time stock update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_17_080347) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_17_082445) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
